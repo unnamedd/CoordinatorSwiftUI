f157a6c22b4ae426326927ca7bd7b9f104a8a211
Thiago Holanda <unnamedd@gmail.com>
2019-08-19 23:56:16 +0200

    Adjusts to UserProfileView

--- CoordinatorCombined WatchKit Extension/Flow/UserProfileView.swift
+++ CoordinatorCombined WatchKit Extension/Flow/UserProfileView.swift
@@ -4,9 +4,27 @@ struct UserProfileView: View {
     var person: Person
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text(person.name)
             Text("Age: \(person.age)")
+            
+            Divider()
+            
+            Text(person.biography)
         }
     }
 }
+
+#if DEBUG
+struct UserProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        let firstPerson = People.makeDummy[0]
+        let secondPerson = People.makeDummy[1]
+        
+        return Group {
+            UserProfileView(person: firstPerson)
+            UserProfileView(person: secondPerson)
+        }
+    }
+}
+#endif
